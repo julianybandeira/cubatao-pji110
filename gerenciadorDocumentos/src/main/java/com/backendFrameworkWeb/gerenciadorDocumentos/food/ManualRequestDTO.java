93de06dae0c58b64addcf354d563420d1c261c99
Leandro De Andrade <45002731+LeandroLDA@users.noreply.github.com>
2023-05-07 21:26:55 -0300

    Carga inicial do backend
    Adição inicial do programa backend e banco de dados.

---
+++ gerenciadorDocumentos/src/main/java/com/backendFrameworkWeb/gerenciadorDocumentos/food/ManualRequestDTO.java
@@ -0,0 +1,4 @@
+package com.backendFrameworkWeb.gerenciadorDocumentos.food;
+
+public record ManualRequestDTO(String nome, String descricao, String link) {
+}
---
+++ gerenciadorDocumentos/src/test/java/com/backendFrameworkWeb/gerenciadorDocumentos/GerenciadorDocumentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backendFrameworkWeb.gerenciadorDocumentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDocumentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
