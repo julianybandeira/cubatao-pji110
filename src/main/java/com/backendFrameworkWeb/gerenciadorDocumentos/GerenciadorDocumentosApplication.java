c0c184dfe5f80d2b013225b43fb4719cb35dc175
LeandroLDA <45002731+LeandroLDA@users.noreply.github.com>
2023-05-07 21:07:13 -0300

    Carga inicial do back end

---
+++ src/main/java/com/backendFrameworkWeb/gerenciadorDocumentos/GerenciadorDocumentosApplication.java
@@ -0,0 +1,13 @@
+package com.backendFrameworkWeb.gerenciadorDocumentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorDocumentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorDocumentosApplication.class, args);
+	}
+
+}
